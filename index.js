e079b7a3519321cbfa5f032f50b6d24df67ea7df
Matt Coburn <mattcoburn24@gmail.com>
2020-03-10 01:30:48 -0700

    added searchbar
    set up the OMDb API and created a search input to fetch movie data

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+	const response = await axios.get('http://www.omdbapi.com', {
+		params: {
+			apikey: '35b27c3',
+			s: searchTerm
+		}
+	});
+
+	console.log(response.data);
+};
+
+let timeoutId;
+const onInput = (event) => {
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+	timeoutId = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 500);
+};
+const input = document.querySelector('input');
+input.addEventListener('input', onInput);
